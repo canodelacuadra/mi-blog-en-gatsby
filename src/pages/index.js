1fe5f69c3ab9b96438e0786fc3afea73462490f3
canodelacuadra <canodelacuadra@gmail.com>
2024-03-16 13:20:41 +0100

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,16 @@
 import * as React from 'react'
-import Layout from '../components/layout'
+import Layout from '../components/layout' 
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
  return (
    <Layout  pageTitle="Inicio">
      <p>Este será nuestro futuro sitio web en el que crearemos un blog con Gatsby</p>
+      <StaticImage
+       alt="Un tandem de exploradores"
+       src="../images/tandem.jpg"
+     />
+
    </Layout>
  )
 }
